fd8815cffe365a48afc54dd326738b4ed8a0d4e8
krpe <peter.kraus@empa.ch>
2022-02-08 16:26:57 +0100

    Startup files.

---
+++ README.md
@@ -0,0 +1,6 @@
+# tomato: au-tomation without pain!
+
+`tomato` is the instrument automation package developed at Empa.
+
+### Contributors:
+- [Peter Kraus](http://github.com/PeterKraus)
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import os
+
+with open("VERSION", "r") as infile:
+    version = infile.read().strip()
+
+with open("README.md", "r", encoding="utf-8") as infile:
+    readme = infile.read()
+
+packagedir = "src"
+
+setuptools.setup(
+    name="tomato",
+    version=version,
+    author="Peter Kraus",
+    author_email="peter@tondon.de",
+    description="au-tomation without pain!",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dgbowl/tomato",
+    project_urls={
+        "Bug Tracker": "https://github.com/dgbowl/tomato/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": packagedir},
+    packages=setuptools.find_packages(where=packagedir),
+    python_requires=">=3.9",
+    install_requires=[
+    ],
+    entry_points={
+    },
+)
