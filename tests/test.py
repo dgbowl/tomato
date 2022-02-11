1725a2647cfb7436e589d632a4e5edef7cd2f4a3
krpe <peter.kraus@empa.ch>
2022-02-11 12:56:48 +0100

    print in tests

--- tests/test.py
+++ tests/test.py
@@ -12,9 +12,10 @@ address = "192.109.209.6"
 channel = 1
 dllpath = "C:\\EC-Lab Development Package\\EC-Lab Development Package\\"
 
-tomato.drivers.biologic.get_status(address, channel, dllpath)
+print(tomato.drivers.biologic.get_status(address, channel, dllpath))
 tomato.drivers.biologic.start_job(address, channel, dllpath, pl)
 time.sleep(10)
-tomato.drivers.biologic.get_data(address, channel, dllpath)
+print(tomato.drivers.biologic.get_status(address, channel, dllpath))
+print(tomato.drivers.biologic.get_data(address, channel, dllpath))
 time.sleep(20)
-tomato.drivers.biologic.get_data(address, channel, dllpath)
+print(tomato.drivers.biologic.get_data(address, channel, dllpath))
