0cd49225aad8082510bdfbdfb8b46aaaf7c8aa17
Peter Kraus <peter.kraus@empa.ch>
2022-04-12 07:59:54 +0200

    Minor changes. (#14)
    * Minor changes.
    
    * Rename E_range, change to yml

--- src/tomato/drivers/biologic/main.py
+++ src/tomato/drivers/biologic/main.py
@@ -95,7 +95,7 @@ def get_data(
     log.debug(f"disconnecting from '{address}:{channel}'")
     api.Disconnect(id_)
     data = parse_raw_data(api, data, device_info.model)
-    return dt.timestamp(), data["data_rows"], data
+    return dt.timestamp(), data["technique"]["data_rows"], data
 
 
 def start_job(
