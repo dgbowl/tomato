a03293d14ef9840808460d22b83202e82cae80ed
Peter Kraus <peter.kraus@geeksonbikes.net>
2022-08-03 08:10:26 +0200

    Wrap `biologic` import in an if-call

--- src/tomato/drivers/biologic/__init__.py
+++ src/tomato/drivers/biologic/__init__.py
@@ -4,4 +4,6 @@
 This driver is a wrapper around BioLogic's `kbio` package. 
 
 """
-from .main import get_status, get_data, start_job, stop_job
+import psutil
+if psutil.WINDOWS:
+    from .main import get_status, get_data, start_job, stop_job
