dd5f5711070334434fd88b5182558cd3d16468a7
Peter Kraus <peter@tondon.de>
2023-12-10 17:14:52 +0100

    lint conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,7 +40,7 @@ def stop_tomato_daemon_session():
     # setup_stuff
     yield
     # teardown_stuff
-    print(f"stop_tomato_daemon_session")
+    print("stop_tomato_daemon_session")
     subprocess.run(["tomato", "stop", "-p", "12345", "--timeout", "1000"])
     if psutil.WINDOWS:
         subprocess.run(["taskkill", "/F", "/IM", "tomato-job", "/T"])
@@ -55,7 +55,7 @@ def stop_tomato_daemon(port: int = 12345):
     # setup_stuff
     yield
     # teardown_stuff
-    print(f"stop_tomato_daemon")
+    print("stop_tomato_daemon")
     subprocess.run(["tomato", "stop", "-p", f"{port}", "--timeout", "1000"])
     if psutil.WINDOWS:
         subprocess.run(["taskkill", "/F", "/IM", "tomato-job", "/T"])
