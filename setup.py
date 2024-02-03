ae04fb0d955339ed2ae92b8e805f4f1d1d46e53e
Peter Kraus <peter.kraus@tu-berlin.de>
2024-02-03 19:05:17 +0100

    Navigate this dependency hell.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
             "sphinx~=7.2",
             "sphinx-rtd-theme~=1.3.0",
             "sphinx-autodoc-typehints < 1.20.0",
-            "autodoc-pydantic~=1.0.0",
+            "autodoc-pydantic==1.9.0",
         ],
     },
     entry_points={
