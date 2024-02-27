b9b92e2233884e62e053ff1b354c0900c308d550
Peter Kraus <peter@tondon.de>
2024-02-27 23:35:47 +0100

    try different ketchup test setup

--- tests/test_02_ketchup.py
+++ tests/test_02_ketchup.py
@@ -133,7 +133,10 @@ def test_ketchup_status_complete(pl, datadir, start_tomato_daemon, stop_tomato_d
 )
 def test_ketchup_cancel(pl, datadir, start_tomato_daemon, stop_tomato_daemon):
     args = [datadir, start_tomato_daemon, stop_tomato_daemon]
-    test_ketchup_status_running(pl, *args)
+    test_ketchup_submit_one(f"{pl}.yml", None, *args)
+    tomato.pipeline_load(**kwargs, pipeline="pip-counter", sampleid=pl)
+    tomato.pipeline_ready(**kwargs, pipeline="pip-counter")
+    wait_until_ketchup_status(jobid=1, status="r", port=PORT, timeout=5000)
     status = tomato.status(**kwargs, with_data=True)
     ret = ketchup.cancel(**kwargs, status=status, verbosity=0, jobids=[1])
     print(f"{ret=}")
@@ -156,7 +159,10 @@ def test_ketchup_cancel(pl, datadir, start_tomato_daemon, stop_tomato_daemon):
 )
 def test_ketchup_snapshot(pl, datadir, start_tomato_daemon, stop_tomato_daemon):
     args = [datadir, start_tomato_daemon, stop_tomato_daemon]
-    test_ketchup_status_running(pl, *args)
+    test_ketchup_submit_one(f"{pl}.yml", None, *args)
+    tomato.pipeline_load(**kwargs, pipeline="pip-counter", sampleid=pl)
+    tomato.pipeline_ready(**kwargs, pipeline="pip-counter")
+    wait_until_ketchup_status(jobid=1, status="r", port=PORT, timeout=5000)
     status = tomato.status(**kwargs, with_data=True)
     ret = ketchup.snapshot(jobids=[1], status=status)
     print(f"{ret=}")
