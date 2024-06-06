6cee2ecff5c7119aaf494d6ec884e756887b5fbd
Peter Kraus <peter.kraus@tu-berlin.de>
2024-06-06 11:21:15 +0200

    Add 0.2.3 to versions.

--- versions.js
+++ versions.js
@@ -4,6 +4,7 @@ document.write('\
 <dl>\
     <dt>Versions</dt> \
     <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../0.2.3/' + page + '">0.2.3</a></dd>\
     <dd><a href="../0.2.1/' + page + '">0.2.1</a></dd>\
 </dl>\
 ');
