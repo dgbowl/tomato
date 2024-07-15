a7871a45cbd7d64559129a7fc9e2a4380f1755c6
Peter Kraus <peter@tondon.de>
2024-07-15 20:00:01 +0200

    master->main

--- versions.js
+++ versions.js
@@ -3,7 +3,7 @@ page = arr[arr.length - 1];
 document.write('\
 <dl>\
     <dt>Versions</dt> \
-    <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../main/' + page + '">main</a></dd>\
     <dd><a href="../0.2.3/' + page + '">0.2.3</a></dd>\
     <dd><a href="../0.2.1/' + page + '">0.2.1</a></dd>\
 </dl>\
