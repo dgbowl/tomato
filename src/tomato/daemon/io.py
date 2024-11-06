a6d58de2d05985f0258aa12889cecdfeabad1be8
Peter Kraus <peter.kraus@tu-berlin.de>
2024-11-06 14:15:52 +0100

    Docs in io module.

--- src/tomato/daemon/io.py
+++ src/tomato/daemon/io.py
@@ -1,6 +1,6 @@
 """
-**tomato.daemon.io**: store and load state of tomato daemon
------------------------------------------------------------
+**tomato.daemon.io**: functions for storing and loading data
+------------------------------------------------------------
 .. codeauthor::
     Peter Kraus
 
@@ -16,18 +16,20 @@ logger = logging.getLogger(__name__)
 
 
 def store(daemon: Daemon):
+    """Stores the status of the provided :class:`Daemon` as a ``pickle`` file."""
     datadir = Path(daemon.settings["datadir"])
     datadir.mkdir(parents=True, exist_ok=True)
     outfile = datadir / f"tomato_state_{daemon.port}.pkl"
-    logger.debug(f"storing daemon state to {outfile}")
+    logger.debug("storing daemon state to '%s'", outfile)
     with outfile.open("wb") as out:
         pickle.dump(daemon, out, protocol=5)
 
 
 def load(daemon: Daemon):
+    """Restores a saved status from a ``pickle`` file into the provided :class:`Daemon`."""
     infile = Path(daemon.settings["datadir"]) / f"tomato_state_{daemon.port}.pkl"
     if infile.exists() is False:
-        logger.debug(f"daemon state file {infile} does not exist")
+        logger.debug("daemon state file '%s' does not exist", infile)
         return
     with infile.open("rb") as inp:
         loaded = pickle.load(inp)
@@ -52,23 +54,24 @@ def merge_netcdfs(jobpath: Path, outpath: Path):
     for fn in jobpath.glob("*.pkl"):
         with pickle.load(fn.open("rb")) as ds:
             datasets.append(ds)
-    logger.debug(f"saving {len(datasets)} as groups")
+    logger.debug("creating a DataTree from %d groups", len(datasets))
     dt = xr.DataTree.from_dict({ds.attrs["role"]: ds for ds in datasets})
+    logger.debug("saving DataTree into '%s'", outpath)
     dt.to_netcdf(outpath, engine="h5netcdf")
 
 
 def data_to_pickle(ds: xr.Dataset, path: Path, role: str):
     """
-    Dump the returned data into a pickle.
-
-    Concatenates with existing data from the same Component.
+    Dumps the data provided as :class:`xr.Dataset` into a ``pickle``. Concatenates with
+    any existing data stored in the ``pickle``.
     """
     logger = logging.getLogger(f"{__name__}.data_to_pickle")
     ds.attrs["role"] = role
-    logger.debug("checking existing")
+    logger.debug("checking for existing pickle at '%s'", path)
     if path.exists():
         with pickle.load(path.open("rb")) as oldds:
+            logger.debug("concatenating Dataset with existing data")
             ds = xr.concat([oldds, ds], dim="uts")
-    logger.debug("dumping pickle")
+    logger.debug("dumping Dataset into pickle at '%s'", path)
     with path.open("wb") as out:
         pickle.dump(ds, out, protocol=5)
