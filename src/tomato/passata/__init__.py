0fea16cd231f99a64129bf9198cc5da1841f1838
Peter Kraus <peter@tondon.de>
2025-02-15 13:07:51 +0100

    Expand functions here.

--- src/tomato/passata/__init__.py
+++ src/tomato/passata/__init__.py
@@ -1,19 +1,15 @@
 import zmq
 from tomato import tomato
-from tomato.models import Reply
+from tomato.models import Reply, Component, Driver
 from typing import Any
 
 
-def component(
-    *,
+def _name_to_cmp(
+    name: str,
     port: int,
     timeout: int,
     context: zmq.Context,
-    name: str,
-    attr: str = None,
-    val: Any = None,
-    **_: dict,
-) -> Reply:
+) -> tuple[Component, Driver]:
     ret = tomato.status(
         port=port, timeout=timeout, context=context, stgrp="tom", yaml=True
     )
@@ -27,28 +23,111 @@ def component(
         )
     cmp = ret.data.cmps[name]
     drv = ret.data.drvs[cmp.driver]
+    return cmp, drv 
+
+
+def status(
+    *,
+    port: int,
+    timeout: int,
+    context: zmq.Context,
+    name: str,
+    **_: dict,
+) -> Reply:
+    ret = _name_to_cmp(name, port, timeout, context)
+    if isinstance(ret, Reply):
+        return ret
+    cmp, drv = ret
+
     kwargs = dict(channel=cmp.channel, address=cmp.address)
     req = context.socket(zmq.REQ)
     req.connect(f"tcp://127.0.0.1:{drv.port}")
-    if attr is None:
-        req.send_pyobj(dict(cmd="dev_status", params={**kwargs}))
-        ret = req.recv_pyobj()
-        req.close()
+    req.send_pyobj(dict(cmd="dev_status", params={**kwargs}))
+    ret = req.recv_pyobj()
+    req.close()
+    return ret
+
+
+def attrs(
+    *,
+    port: int,
+    timeout: int,
+    context: zmq.Context,
+    name: str,
+    **_: dict,
+) -> Reply:
+    ret = _name_to_cmp(name, port, timeout, context)
+    if isinstance(ret, Reply):
         return ret
+    cmp, drv = ret
+
+    kwargs = dict(channel=cmp.channel, address=cmp.address)
+    req = context.socket(zmq.REQ)
+    req.connect(f"tcp://127.0.0.1:{drv.port}")
     req.send_pyobj(dict(cmd="attrs", params={**kwargs}))
     ret = req.recv_pyobj()
-    if attr is True:
+    return ret
+
+
+def capabilities(
+    *,
+    port: int,
+    timeout: int,
+    context: zmq.Context,
+    name: str,
+    **_: dict,
+) -> Reply:
+    ret = _name_to_cmp(name, port, timeout, context)
+    if isinstance(ret, Reply):
         return ret
-    if attr not in ret.data:
-        return Reply(
-            success=False,
-            msg=f"attr {attr!r} not accessible on component {name!r}",
-            data=ret.data,
-        )
-    if val is None:
-        req.send_pyobj(dict(cmd="dev_get_attr", params={"attr": attr, **kwargs}))
-        ret = req.recv_pyobj()
+    cmp, drv = ret
+
+    kwargs = dict(channel=cmp.channel, address=cmp.address)
+    req = context.socket(zmq.REQ)
+    req.connect(f"tcp://127.0.0.1:{drv.port}")
+    req.send_pyobj(dict(cmd="capabilities", params={**kwargs}))
+    ret = req.recv_pyobj()
+    return ret
+
+
+def get_attr(
+    port: int,
+    timeout: int,
+    context: zmq.Context,
+    name: str,
+    attr: str,
+    **_: dict,
+) -> Reply:
+    ret = _name_to_cmp(name, port, timeout, context)
+    if isinstance(ret, Reply):
         return ret
+    cmp, drv = ret
+
+    kwargs = dict(channel=cmp.channel, address=cmp.address)
+    req = context.socket(zmq.REQ)
+    req.connect(f"tcp://127.0.0.1:{drv.port}")
+    req.send_pyobj(dict(cmd="dev_get_attr", params={"attr": attr, **kwargs}))
+    ret = req.recv_pyobj()
+    return ret
+
+
+def set_attr(
+    port: int,
+    timeout: int,
+    context: zmq.Context,
+    name: str,
+    attr: str,
+    val: Any,
+    **_: dict,
+) -> Reply:
+    ret = _name_to_cmp(name, port, timeout, context)
+    if isinstance(ret, Reply):
+        return ret
+    cmp, drv = ret
+
+    kwargs = dict(channel=cmp.channel, address=cmp.address)
+    req = context.socket(zmq.REQ)
+    req.connect(f"tcp://127.0.0.1:{drv.port}")
     req.send_pyobj(
         dict(cmd="dev_set_attr", params={"attr": attr, "val": val, **kwargs})
     )
@@ -59,63 +138,60 @@ def component(
 if __name__ == "__main__":
     import random
 
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    kwargs = dict(port=1234, timeout=1000, context=zmq.Context())
+    ret = status(
         name="example_counter:(example-addr,1)",
+        **kwargs,
     )
     print(ret.data)
-    ret = component(
-        port=1234, timeout=1000, context=zmq.Context(), name="psutil:(psutil-addr,1)"
-    )
-    print(ret.data)
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    ret = status(
         name="psutil:(psutil-addr,1)",
-        attr=True,
+        **kwargs,
     )
     print(ret.data)
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    ret = attrs(
+        name="psutil:(psutil-addr,1)",
+        **kwargs,
+    )
+    print(ret.data)
+    ret = capabilities(
+        name="psutil:(psutil-addr,1)",
+        **kwargs,
+    )
+    
+    print(ret.data)
+    ret = get_attr(
         name="psutil:(psutil-addr,1)",
         attr="cpu_count",
+        **kwargs,
     )
     print(ret.data)
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    ret = attrs(
         name="example_counter:(example-addr,1)",
-        attr=True,
+        **kwargs,
     )
     print(ret.data)
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    ret = capabilities(
+        name="example_counter:(example-addr,1)",
+        **kwargs,
+    )
+    print(ret.data)
+    ret = get_attr(
         name="example_counter:(example-addr,1)",
         attr="max",
+        **kwargs,
     )
     print(ret.data)
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    ret = set_attr(
         name="example_counter:(example-addr,1)",
         attr="max",
         val=random.random() * 10,
+        **kwargs
     )
     print(ret.data)
-    ret = component(
-        port=1234,
-        timeout=1000,
-        context=zmq.Context(),
+    ret = get_attr(
         name="example_counter:(example-addr,1)",
         attr="max",
+        **kwargs
     )
     print(ret.data)
